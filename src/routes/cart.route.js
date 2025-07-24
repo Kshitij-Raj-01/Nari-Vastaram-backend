383ec3873c8a7abc7a5eae2c051d213fe39ef637
Kshitij Raj <155631442+Kshitij-Raj-01@users.noreply.github.com>
2025-07-24 23:03:58 +0530

    Update cart.route.js

--- src/routes/cart.route.js
+++ src/routes/cart.route.js
@@ -6,5 +6,6 @@ const authenticate = require("../middleware/authenticate");
 
 router.get("/", authenticate, cartController.findUserCart);
 router.put("/add", authenticate, cartController.addItemToCart);
+router.post("/merge", authMiddleware, cartController.mergeGuestCart);
 
 module.exports = router;
